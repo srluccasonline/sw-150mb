e54182c4864b7b72e17a4bcb0f876b3632054623
Luccas Brandão <luccasbrandao1996@gmail.com>
2024-11-18 06:24:21 -0300

    Git 150mb

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'pwa-test-cache-v1';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            const urlsToCache = Array.from({ length: 30 }, (_, i) => 
+                `/test-files/test-file-${i + 1}.txt`
+            );
+            console.log('Iniciando cache dos arquivos...');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                console.log('Arquivo encontrado no cache:', event.request.url);
+                return response;
+            }
+            console.log('Arquivo não encontrado no cache:', event.request.url);
+            return fetch(event.request);
+        })
+    );
+});
